8611e16545919c6a9ad13c70cf68d8b9763185bd
surbhi kumari <surbhikumaridav@gmail.com>
2022-06-29 21:44:33 +0530

    sign component , authentication complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
-import SignIn from './routes/sign-in/sign-in.component'
+import Authentication from './routes/authentication/authentication.component'
 const Shop = () =>{
   return <h1>I am the shop pages</h1>
 }
@@ -12,7 +12,7 @@ const App = () => {
     <Route path='/' element={<Navigation/>}>
       <Route index element={<Home />} />
       <Route path='/shop' element={<Shop />} />
-      <Route path='sign-in' element={<SignIn />} />
+      <Route path='auth' element={<Authentication />} />
     </Route>
    </Routes>
  )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
     signInWithRedirect,
     signInWithPopup, 
     GoogleAuthProvider ,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -64,4 +65,8 @@ const firebaseConfig = {
     return await createUserWithEmailAndPassword(auth,email,password)
   }
 
+  export const signInAuthUserWithEmailAndPassword =async(email,password)=>{
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth,email,password)
+  }
   
