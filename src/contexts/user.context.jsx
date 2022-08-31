b3683a38ded2c4343e06319eac184811a15911a2
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-08-31 11:08:20 +0530

    Update user.context.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,18 @@
 import { createContext,useState,useEffect } from "react";
 import { onAuthStateChangedListener,createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 //as the actual value ypu want to access
+//default value in create context
 export const UserContext=createContext({
+    //set null value whether the user object exist or not there is no context when there is no user
     currentUser:null,
+    //empty function
     setCurrentUser:()=>null
 })
 
+//provider is a actual component that receive children return user context dot provider
 export const UserProvider=({children})=>{
     const [currentUser,setCurrentUser]=useState(null)
+    //an object that passes the two values 
     const value={currentUser,setCurrentUser}
     
     useEffect(()=>{
@@ -19,5 +24,9 @@ export const UserProvider=({children})=>{
         })
         return unsubscribe
     },[])
+    
+     //the provider is the component that will wrap around any other components that need access to the values inside that are just going to render the children.
+    //provider receive the value, which is going to hold the actual contextual values.
+    // provider is essentially allowing any of its child components to access the values inside of its used state.
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
