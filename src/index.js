ceabf06304c013087d9d6fd3cd62f8d7df570f8d
Surbhi Kumari <surbhikumaridav@gmail.com>
2022-09-02 16:19:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-  //user provider nested deep within the app can access the context value inside of the provider itself. Anything outside will not be able to access the context.
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
